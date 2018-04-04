3f81b8137c4ef5c60c88a41e2cf8f5e6c67b4bbb
Anderson S Barros <anderson21br@gmail.com>
2018-04-04 13:19:08 -0300

    Operador while

---
+++ operadores-php7/operadores_ex06_while.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>O operador while</title>
+  </head>
+  <body>
+  <?php
+    
+    $i = 0
+    while($i < 5){
+      
+      echo $i . " ";
+      $i++;
+    }
+     
+  // Aula #2.2 (Prática): A Linguagem PHP ; em 50min
+  ?>
+  </body>
+<html>
