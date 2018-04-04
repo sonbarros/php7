16852a89f9c3d543d5374d8e3b51d1ddb3891c81
Anderson S Barros <anderson21br@gmail.com>
2018-04-04 13:24:14 -0300

    Operador do{}while()

---
+++ operadores-php7/operadores_ex07_do-while.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>O operador do{}while()</title>
+  </head>
+  <body>
+  <?php
+    
+    $i = 0
+    do{
+      echo $i . " ";
+      $i++;
+    }while($i < 5)
+     
+  // Aula #2.2 (Prática): A Linguagem PHP ; em 55min
+  ?>
+  </body>
+<html>
