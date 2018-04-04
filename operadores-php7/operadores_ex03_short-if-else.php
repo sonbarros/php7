e9e270a18ed5ae08f44e08dfd97ce80a511b7473
Anderson S Barros <anderson21br@gmail.com>
2018-04-04 00:57:47 -0300

    Operador short-if - VERIFICANDO SE O VALOR E PAR OU IMPAR

--- operadores-php7/operadores_ex03_short-if-else.php
+++ operadores-php7/operadores_ex03_short-if-else.php
@@ -4,11 +4,15 @@
   </head>
   <body>
   <?php
-    
+    // O operador começa com uma condição, e atribui o valor em $x
+
     $x = 10 / 2 == 5 ? "É cinco.<BR>" : "Não é cinco.<BR>";
     echo $x;
 
-    // O operador começa com uma condição, e atribui o valor em $x
+    // Verificando se $y é PAR ou IMPAR
+    $y = 13;
+    $x = $y%2 == 0 ? "PAR.<BR>" : "ÍMPAR.<BR>";
+    echo $x;
      
   // Aula #2.2 (Prática): A Linguagem PHP 
   ?>
