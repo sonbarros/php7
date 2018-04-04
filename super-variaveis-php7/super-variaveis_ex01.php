3f067cfa07cde2df41d967cd02e34df91b6bfcf6
Anderson S Barros <anderson21br@gmail.com>
2018-04-04 13:38:04 -0300

    Super variaveis do php

---
+++ super-variaveis-php7/super-variaveis_ex01.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Super Variaveis do Php</title>
+  </head>
+  <body>
+  <?php
+      
+      echo $_SERVER["SERVER_ADDR"]; // informar o IP do servidor
+      echo $_SERVER["SERVER_NAME"]; // informar o nome da maquina servidor
+      echo $_SERVER["HTTP_USER_AGENT"]; // informa detalhes sobre o navegador
+      echo $_SERVER["REMOTE_ADDR"]; // informar o IP so usuario
+    
+     
+  // Aula #2.2 (Prática): A Linguagem PHP ; em 60min
+  ?>
+  </body>
+<html>
