f6da816416e9dce972a026bd4e6ff2fa579863c1
Anderson S Barros <anderson21br@gmail.com>
2018-04-04 00:54:20 -0300

    Operador short-if

---
+++ operadores-php7/operadores_ex03_short-if-else.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+  <?php
+    
+    $x = 10 / 2 == 5 ? "É cinco.<BR>" : "Não é cinco.<BR>";
+    echo $x;
+
+    // o operador começa com uma condição, e atribui o valor em $x
+     
+  // Aula #2.2 (Prática): A Linguagem PHP 
+  ?>
+  </body>
+<html>
