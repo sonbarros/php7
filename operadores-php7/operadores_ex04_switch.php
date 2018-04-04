30c202b4a8fb519d1c350f54e48abe7ea57b02ed
Anderson S Barros <anderson21br@gmail.com>
2018-04-04 01:03:51 -0300

    Operador switch

---
+++ operadores-php7/operadores_ex04_switch.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>O operador switch</title>
+  </head>
+  <body>
+  <?php
+    
+    switch($z){
+      case 0:
+        echo "O valor de Z é 0";
+        break;
+      case 1:
+        echo "O valor de Z é 1";
+        break;
+      case 2:
+        echo "O valor de Z é 0";
+        break;
+      default:
+        echo "Nenhum";
+        break;
+    }
+     
+  // Aula #2.2 (Prática): A Linguagem PHP 
+  ?>
+  </body>
+<html>
