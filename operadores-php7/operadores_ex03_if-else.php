3d09fb3dea380f56aedb24398d4b5f088ff0ccad
Anderson S Barros <anderson21br@gmail.com>
2018-04-04 00:49:08 -0300

    Operador condicional if-else testando condicao com && e ++$

---
+++ operadores-php7/operadores_ex03_if-else.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+  <?php
+    
+    $x = 5;
+
+    if($x == 5 && ++$x < 4)
+    {
+        echo "Condição do comando IF foi aceita.<BR>";
+    }
+    else
+    {
+        echo "NEGADO <BR>";
+    }
+    echo $x . "<BR>"; // Resultado: 5
+
+    // atenção para os operadores duplos : &&, || 
+     
+  // Aula #2.2 (Prática): A Linguagem PHP 
+  ?>
+  </body>
+<html>
