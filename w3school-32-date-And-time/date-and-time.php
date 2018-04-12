39c70811669ba56a032503f5c85ce5910a0795ee
Anderson S Barros <anderson21br@gmail.com>
2018-04-12 18:44:18 -0300

    Date time

--- w3school-32-date-And-time/date-and-time.php
+++ w3school-32-date-And-time/date-and-time.php
@@ -41,7 +41,29 @@ a - Lowercase Ante meridiem and Post meridiem (am or pm)
 date_default_timezone_set("America/New_York");
 echo "The time is " . date("h:i:sa");
 ?>
+
+<?php
+// Criar uma data com PHP mktime ()
+$d = mktime(11, 14, 54, 8, 12, 2014);
+echo "Created date is " . date("Y-m-d h:i:sa", $d);
 ?>
 
+<?php
+// Converte uma string para data 
+$d = strtotime("10:30pm April 15 2014");
+echo "Created date is " . date("Y-m-d h:i:sa", $d);
+
+// PHP é bastante inteligente sobre a conversão de uma string para uma data, 
+// assim você pode colocar em vários valores:
+
+$d=strtotime("tomorrow");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("next Saturday");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+
+$d=strtotime("+3 Months");
+echo date("Y-m-d h:i:sa", $d) . "<br>";
+?>
 
 
