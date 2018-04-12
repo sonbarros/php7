b390a554488dd1daa25f5d088e56c933be632aca
Anderson S Barros <anderson21br@gmail.com>
2018-04-12 18:43:54 -0300

    Array bidimensional

---
+++ w3school-16-arrays/array-bidimensional.php
@@ -0,0 +1,21 @@
+<?php
+$cars = array
+(
+array("Volvo",22,18),
+array("BMW",15,13),
+array("Saab",5,2),
+array("Land Rover",17,15)
+);
+
+echo $cars[0][0].": In stock: ".$cars[0][1].", sold: ".$cars[0][2].".<br>";
+echo $cars[1][0].": In stock: ".$cars[1][1].", sold: ".$cars[1][2].".<br>";
+echo $cars[2][0].": In stock: ".$cars[2][1].", sold: ".$cars[2][2].".<br>";
+echo $cars[3][0].": In stock: ".$cars[3][1].", sold: ".$cars[3][2].".<br>";
+
+/* resultado
+Volvo: In stock: 22, sold: 18.
+BMW: In stock: 15, sold: 13.
+Saab: In stock: 5, sold: 2.
+Land Rover: In stock: 17, sold: 15.
+*/
+?>
